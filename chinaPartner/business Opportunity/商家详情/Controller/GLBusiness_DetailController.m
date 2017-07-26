b93b082961128b998a60963e82cb312175311bb1
龚磊 <1029235277@qq.com>
2017-07-26 13:37:47 +0800

    no message

---
+++ chinaPartner/business Opportunity/商家详情/Controller/GLBusiness_DetailController.m
@@ -0,0 +1,25 @@
+//
+//  GLBusiness_DetailController.m
+//  chinaPartner
+//
+//  Created by 龚磊 on 2017/7/26.
+//  Copyright © 2017年 四川三君科技有限公司. All rights reserved.
+//
+
+#import "GLBusiness_DetailController.h"
+
+@interface GLBusiness_DetailController ()
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation GLBusiness_DetailController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+@end
